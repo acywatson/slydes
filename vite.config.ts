e3e1bf7e111512d7289c1fc89c53cff72e96d938
Acy Watson <acy.watson@gmail.com>
2022-11-03 00:34:59 -0700

    add script

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import * as packageJson from './package.json'
+
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
   plugins: [
